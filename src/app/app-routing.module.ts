696ec38a9a0e854cd86b2757fc791c220a3faa8e
Einsenhorm <einsenhorm@gmail.com>
2023-01-03 16:47:52 +0100

    commit message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormulaireComponent } from './formulaire/formulaire.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { PremierComposantComponent } from './premier-composant/premier-composant.component';
 
-const routes: Routes = [];
+
+// declaration des routes pour afficher le contenu des pages
+const routes: Routes = [
+  {path: "", component: PremierComposantComponent},
+  {path: "form", component: FormulaireComponent},
+  {path: "404", component: NotFoundComponent},
+  //tjr placer en dernier sinon les routes ne seront pas prises en compte
+  {path: "**", redirectTo: '/404'}
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/premier-composant/premier-composant.component.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+import { User } from '../interfaces/user';
+
+
+@Component({
+  selector: 'app-premier-composant',
+  templateUrl: './premier-composant.component.html',
+  styleUrls: ['./premier-composant.component.css']
+})
+export class PremierComposantComponent {
+  etudiant: number = 322;
+  dir:any = {prenom:'Adrien' };
+  user : User = {id: 1, name: 'Jhon Wick', email: 'jhon.wick@adressebidon.zeuby'};
+  // affiche un pop up sur la page web
+  afficheMessage(message: string): void {
+    alert("mon message: " +message);
+  }
+  afficheMessage2(message: string, event:any): void {
+    alert("mon message: " +message);
+    event.preventDefault();
+    event.stopPropagation();
+  }
+  quantites: number[] = [12, 189, 17, 12];
+  
+  
+  user1: User = { id: 1, name: 'John', email: 'john@example.com' };
+  user2: User = { id: 2, name: 'Jane', email: 'jane@example.com' };
+  user3: User = { id: 3, name: 'Bob', email: 'bob@example.com' };
+  users: User[] = [this.user1, this.user2, this.user3];
+  
+  data: any[] = [{
+    "id": 1,
+    "name": "abc",
+    "address": {
+      "streetName": "cde",
+      "streetId": 2
+    },
+    "projets": [
+      {
+      "libelle":"lib1",
+      "description":"desc1"
+      },
+      {
+      "libelle":"lib2",
+      "description":"desc2"
+      }
+  ]
+  },
+  {
+    "id": 2,
+    "name": "abc",
+    "address": {
+      "streetName": "cde",
+      "streetId": 2
+    },
+    "projets": [
+      {
+      "libelle":"lib1",
+      "description":"desc1"
+      },
+      {
+      "libelle":"lib2",
+      "description":"desc2"
+      }
+  ]
+  }]
+  position : number= 2;
+
+}
